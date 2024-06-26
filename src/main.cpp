a70977f2efbf6e27eb3d267e4397f3a16be83393
IvoBlanc <ivoblanc99@gmail.com>
2024-06-26 18:46:44 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <Arduino.h>
+
+const int ledPin = 2;  // Pin donde está conectado el LED
+const int interval = 1000;  // Intervalo de tiempo en milisegundos para el parpadeo
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // Configurar el pin como salida
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);  // Encender el LED
+  delay(interval);  // Esperar
+  digitalWrite(ledPin, LOW);  // Apagar el LED
+  delay(interval);  // Esperar
+
+  printf ("sssss");
+}
